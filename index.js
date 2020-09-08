520a28de363fd6ed1095959552d72d46f99a0c19
aimforbigfoot <61890796+aimforbigfoot@users.noreply.github.com>
2020-09-07 23:03:10 -0400

    Got the buttons working
    BIg brained

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const bar = document.querySelector("input");
 const listOfEntries = document.querySelector(".list-of-things")
 const subBtn = document.querySelector(".sub-btn");
-const maxCharLength = 40;
+const warningText = document.querySelector(".warning-text")
+const listOfItem = document.querySelector(".list-item")
+const maxCharLength = 10;
+var activeBtn
 var entries = [];
+var currentID = 0;
 
 
 
@@ -12,24 +16,44 @@ bar.addEventListener("keyup", (e) => {
     }
 })
 
-
 subBtn.addEventListener("click", () => {
     submit_btn_func();
 })
 
+function delClicked (e) {
+    console.log(e)
+    e.parentElement.remove()
+}
+
+
+
 function submit_btn_func() {
-    if (bar.value.length < maxCharLength){
+    if (bar.value.length < maxCharLength && bar.value.length > 0){
         let text = bar.value;
         entries.push(text)
         bar.value = "";
-        listOfEntries.innerHTML += `<div class="list-of-things">
-        <div class="list-item">
-            <p class="list-item-text">${text}</p>
-            <button class="list-item-button">Delete</button>
+        listOfEntries.innerHTML += `<div id="${entries.length}" class="list-item">
+        <p class="list-item-text">${text}</p>
+        <button class="list-item-button" onclick="delClicked(this)">Delete</button>
     </div>`
-    
+        console.log(bar.value.length)
         console.log(entries);
+    } else if (bar.value.length >= maxCharLength){
+        const errText = `Exceded character amount of ${maxCharLength}`
+        show_error_text(errText)
+    } else if (bar.value.length == 0) {
+        const errText = "There has to be something you are grateful for";
+        show_error_text(errText);
     } else{
-        
+        const errText = "Undefind Error";
+        show_error_text("undefined error please contact");
     }
+}
+
+function show_error_text(errTextVar) {
+    console.log(errTextVar);
+    warningText.innerHTML = errTextVar
+
+    window.setTimeout(() => {warningText.innerHTML = " "},5000)
+    
 }
